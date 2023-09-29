aa570d1650aca06ad564cf9a6862285cba5598f1
Omar_elhalabi <112423459+Omarelhalabi54@users.noreply.github.com>
2023-09-29 18:25:53 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+var namer = document.getElementById("name");
+namer.addEventListener("click", function(){
+    alert("Welcome to Homepage");
+});
+function tog() {
+    document.getElementById("para").innerHTML = "This aspect of AI programming is concerned with gathering data and formulating the rules necessary to transform it into useful knowledge. The guidelines, also known as algorithms, give computing equipment detailed instructions on how to carry out a certain activity.";
+};
+function reas() {
+    document.getElementById("r").innerHTML = "This aspect of AI programming is concerned with selecting the best algorithm to achieve a particular result.";
+};
+function sel() {
+    document.getElementById("s").innerHTML = "This aspect of AI programming is to continuously improve algorithms and make sure they deliver the most precise results.";
+};
+function cre() {
+    document.getElementById("c").innerHTML = "To create new images, texts, songs, and ideas, this aspect of AI uses neural networks, rules-based systems, statistical techniques, and other AI tools.";
+};
